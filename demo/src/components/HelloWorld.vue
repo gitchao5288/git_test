7e076296ad2f54d928a716746f436199ed90efd6
jiangchao <jiangchao456@foxmail.com>
2019-06-04 13:43:39 +0800

    vue第六次上传

--- demo/src/components/HelloWorld.vue
+++ demo/src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-	<h1>你好,PHP是世界上最好的语言!</h1>
+	<h1>你好,请问PHP是世界上最好的语言吗?</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
