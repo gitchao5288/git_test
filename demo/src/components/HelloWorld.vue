46489de91252558c43bc9935aae2932dca95a827
jiangchao <jiangchao456@foxmail.com>
2019-06-04 10:59:51 +0800

    vue第五次上传

--- demo/src/components/HelloWorld.vue
+++ demo/src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-	<h1>你好,Git</h1>
+	<h1>你好,我是姜超</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
